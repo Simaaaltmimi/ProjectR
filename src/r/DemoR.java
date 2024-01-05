2fc71a41ce96d0a68d37d3bc61117b2eda7f9a47
simaa <simaa@ahmeds-mbp.lan>
2024-01-04 23:12:49 -0500

    initial commit

---
+++ src/r/DemoR.java
@@ -0,0 +1,10 @@
+package r;
+
+public class DemoR {
+	public static void main(String[] args) {
+		System.out.println("Monday");
+		System.out.println("Sprint 1 work..");
+		System.out.println("Sprint 2 work..");
+	}
+
+}
